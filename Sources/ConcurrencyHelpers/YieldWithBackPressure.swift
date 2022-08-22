39ce7957d50c1b68dddbd5813bc3f91257e42a43
mr-swifter <103502437+mr-swifter@users.noreply.github.com>
2022-08-22 13:29:17 +0300

    feat: [sc-285] add yield with back pressure helper (#15)
    * feat: [sc-285] add yield with back pressure helper

---
+++ Sources/ConcurrencyHelpers/YieldWithBackPressure.swift
@@ -0,0 +1,29 @@
+/// Yields the `message` to the AsyncStream `continuation`
+/// with back pressure logic.
+///
+/// If `continuation` returns `.dropped`, the method yields the Task and try again.
+///
+/// - Parameter message: The value to yield to the continuation.
+/// - Parameter continuation: The continuation to yield message to.
+/// - Returns: `true` if the `message` has been succesfully yielded to the stream and `false` in otherwise.
+@discardableResult
+public func yieldWithBackPressure<Message>(message: Message,
+                                           to continuation: AsyncStream<Message>.Continuation) async -> Bool {
+    while true {
+        let result = continuation.yield(message)
+        switch result {
+        case .terminated:
+            // Stream is closed
+            return false
+        case .enqueued:
+            // Here we can know how many slots remains in the stream
+            return true
+        case .dropped:
+            // Here we can know what message has beed dropped
+            await Task.yield()
+            continue
+        @unknown default:
+            fatalError("Runtime error: unknown case in \(#function), \(#file):\(#line)")
+        }
+    }
+}
