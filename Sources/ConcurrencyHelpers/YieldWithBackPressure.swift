f09cc1dc913e8de3716600c00e503c7faef14733
mr-swifter <103502437+mr-swifter@users.noreply.github.com>
2022-08-22 12:30:07 +0300

    Return result of yield operation

--- Sources/ConcurrencyHelpers/YieldWithBackPressure.swift
+++ Sources/ConcurrencyHelpers/YieldWithBackPressure.swift
@@ -1,14 +1,24 @@
-public func yieldWithBackPressure<Message>(message: Message, to continuation: AsyncStream<Message>.Continuation) async {
+/// Yields the `message` to the AsyncStream `continuation`
+/// with back pressure logic.
+///
+/// If `continuation` returns `.dropped`, the method yields the Task and try again.
+///
+/// - Parameter message: The value to yield to the continuation.
+/// - Parameter continuation: The continuation to yield message to.
+/// - Returns: `true` if the `message` has been succesfully yielded to the stream and `false` in otherwise.
+public func yieldWithBackPressure<Message>(message: Message,
+                                           to continuation: AsyncStream<Message>.Continuation) async -> Bool {
     var enqueued = false
     while !enqueued {
         let result = continuation.yield(message)
         switch result {
         case .terminated:
             // Stream is closed
-            return
+            return false
         case .enqueued:
             // Here we can know how many slots remains in the stream
             enqueued = true
+            return true
         case .dropped:
             // Here we can know what message has beed dropped
             await Task.yield()
