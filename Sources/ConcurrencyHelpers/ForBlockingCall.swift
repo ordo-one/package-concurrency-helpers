a9882d0442b3fba2d9b3b6bf43f0bdfa82d30da4
mr-swifter <103502437+mr-swifter@users.noreply.github.com>
2022-09-14 17:15:10 +0300

    feat: [sc-668][sc-669] add helper method for calling blocking functio… (#17)
    * feat: [sc-668][sc-669] add helper method for calling blocking function fro, async

---
+++ Sources/ConcurrencyHelpers/ForBlockingCall.swift
@@ -0,0 +1,29 @@
+import Dispatch
+
+/// This function is a helper for calling blocking function from Task
+/// and properly suspend the Task while such call take place.
+/// The blocking function call happens in global dispatch queue
+/// and when it's done calls continuation to resume suspended Task.
+///
+/// #### Usage example:
+///  ```swift
+///  Task {
+///      let result = await forBlockingFunc {
+///          myBlockingFunction()
+///      }
+///      // end here only when myBlockingFunction() is done
+///  }
+///  ```
+///
+/// - Parameter queue: The queue where blocking call will be invoked
+/// - Parameter body: Function to be called in the `queue`
+///
+/// - Returns: The function returns a value returned by `body` function
+public func forBlockingFunc<T>(queue _: DispatchQueue = .global(),
+                               body: @escaping () -> T) async -> T {
+    await withCheckedContinuation { continuation in
+        DispatchQueue.global().async {
+            continuation.resume(returning: body())
+        }
+    }
+}
