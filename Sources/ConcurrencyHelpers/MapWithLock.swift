53ec288cd6b6c6ad7835429b9b443ea4aa9d5621
mr-swifter <103502437+mr-swifter@users.noreply.github.com>
2022-10-11 14:44:46 +0300

    feat: [sc-741] add helper for map with lock

---
+++ Sources/ConcurrencyHelpers/MapWithLock.swift
@@ -0,0 +1,20 @@
+public struct MapWithLock<K: Hashable, V> {
+    var lock: ConcurrencyHelpers.Lock = .init()
+    var map: [K: V] = [:]
+
+    public subscript(key: K) -> V? {
+        get {
+            lock.withLock {
+                guard let value = map[key] else {
+                    return nil
+                }
+                return value
+            }
+        }
+        set {
+            lock.withLock {
+                map[key] = newValue
+            }
+        }
+    }
+}
