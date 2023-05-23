cefdf6df74f882b1a7b4680857bdc29dcb5feffe
dimlio <122263440+dimlio@users.noreply.github.com>
2023-05-23 11:50:40 +0300

    add a warning comment

--- Sources/ConcurrencyHelpers/RunSync.swift
+++ Sources/ConcurrencyHelpers/RunSync.swift
@@ -2,6 +2,11 @@ import Dispatch
 
 /**
  * Runs async closure and waits for its result in non-async code.
+ *
+ * Use with care as this blocks the current thread and violates
+ * "forward progress" Swift concurrency runtime contract.
+ *
+ * See https://developer.apple.com/videos/play/wwdc2021/10254/?time=1582
  */
 public func runSync<T>(_ closure: @escaping () async -> T) -> T {
     let result = UnsafeMutableTransferBox<T?>(nil)
@@ -21,6 +26,11 @@ public func runSync<T>(_ closure: @escaping () async -> T) -> T {
 /**
  * Runs throwing async closure and waits for its result (incl. rethrowing exception)
  * in non-async code.
+ *
+ * Use with care as this blocks the current thread and violates
+ * "forward progress" Swift concurrency runtime contract.
+ *
+ * See https://developer.apple.com/videos/play/wwdc2021/10254/?time=1582
  */
 public func runSync<T>(_ closure: @escaping () async throws -> T) throws -> T{
     func nonthowing() async -> Result<T, Error> {
