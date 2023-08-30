185e92eccdeb6431559c2631acf67ac01fe2bdcd
Axel Andersson <axel@ordo.one>
2023-08-30 13:41:58 +0300

    Move file, add license header

--- Sources/Helpers/MakeStream.swift
+++ Sources/Helpers/MakeStream.swift
@@ -1,3 +1,11 @@
+// Copyright 2023 Ordo One AB
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+
 /// `makeStream` is part of Swift 5.9
 /// https://github.com/apple/swift-evolution/blob/main/proposals/0388-async-stream-factory.md
 public func makeStream<T>(of _: T.Type) -> (AsyncStream<T>, AsyncStream<T>.Continuation) {
