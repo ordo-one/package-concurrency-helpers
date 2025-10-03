f99daec62fc21ae13d6a0b5312c4125976ec9036
BlindSpot <127803250+blindspotbounty@users.noreply.github.com>
2025-10-03 10:52:36 +0200

    create closure before condition

--- Sources/ConcurrencyHelpers/RunSync.swift
+++ Sources/ConcurrencyHelpers/RunSync.swift
@@ -23,41 +23,26 @@ public func runSync<T>(priority: TaskPriority? = nil, _ operation: () async -> T
 
         let wrapper = UnsafeMutableTransferBox(Optional(escapableOperation))
 
+        let body = { [wrapper] in
+            result.wrappedValue = await wrapper.wrappedValue!()
+
+            // Make sure reference to escapableOperation is released here otherwise
+            // it can outlive withoutActuallEscaping() block.
+            wrapper.wrappedValue = nil
+
+            semaphore.signal()
+        }
         #if compiler(>=6.2)
             // preferably to have
             // if #compiler (>=6.2) && #available(macOS 26, iOS 26, *)
             // but that is not supported by swift...
             if #available(macOS 26, iOS 26, *) {
-                Task.immediate(priority: priority) { [wrapper] in
-                    result.wrappedValue = await wrapper.wrappedValue!()
-
-                    // Make sure reference to escapableOperation is released here otherwise
-                    // it can outlive withoutActuallEscaping() block.
-                    wrapper.wrappedValue = nil
-
-                    semaphore.signal()
-                }
+                Task.immediate(priority: priority, operation: body)
             } else {
-                Task(priority: priority) { [wrapper] in
-                    result.wrappedValue = await wrapper.wrappedValue!()
-
-                    // Make sure reference to escapableOperation is released here otherwise
-                    // it can outlive withoutActuallEscaping() block.
-                    wrapper.wrappedValue = nil
-
-                    semaphore.signal()
-                }
+                Task(priority: priority, operation: body)
             }
         #else
-            Task(priority: priority) { [wrapper] in
-                result.wrappedValue = await wrapper.wrappedValue!()
-
-                // Make sure reference to escapableOperation is released here otherwise
-                // it can outlive withoutActuallEscaping() block.
-                wrapper.wrappedValue = nil
-
-                semaphore.signal()
-            }
+            Task(priority: priority, operation: body)
         #endif
         semaphore.wait()
     }
